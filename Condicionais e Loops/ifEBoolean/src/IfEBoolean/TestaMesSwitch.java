74d8a0d701486a37dae8e94edba93fcc25a21da4
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-01-14 17:16:31 -0300

    Switch - Testa Mês

---
+++ Condicionais e Loops/ifEBoolean/src/IfEBoolean/TestaMesSwitch.java
@@ -0,0 +1,52 @@
+package IfEBoolean;
+
+public class TestaMesSwitch {
+
+        public static void main(String[] args) {
+
+            int mes = 10;
+
+            switch (mes) {
+                case 1:
+                    System.out.println("O mês é Janeiro");
+                    break;
+                case 2:
+                    System.out.println("O mês é Fevereiro");
+                    break;
+                case 3:
+                    System.out.println("O mês é Março");
+                    break;
+                case 4:
+                    System.out.println("O mês é Abril");
+                    break;
+                case 5:
+                    System.out.println("O mês é Maio");
+                    break;
+                case 6:
+                    System.out.println("O mês é Junho");
+                    break;
+                case 7:
+                    System.out.println("O mês é Julho");
+                    break;
+                case 8:
+                    System.out.println("O mês é Agosto");
+                    break;
+                case 9:
+                    System.out.println("O mês é Setembro");
+                    break;
+                case 10:
+                    System.out.println("O mês é Outubro");
+                    break;
+                case 11:
+                    System.out.println("O mês é Novembro");
+                    break;
+                case 12:
+                    System.out.println("O mês é Dezembro");
+                    break;
+                default:
+                    System.out.println("Mês inválido");
+                    break;
+            }
+        }
+    }
+
