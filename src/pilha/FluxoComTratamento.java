3f03d73e961ffa5bee121ec62e691eadb9929847
Audrey-Albuquerque <audreydev.br@gmail.com>
2023-08-06 08:54:13 -0300

    :rowing_woman: Indentação

--- src/pilha/FluxoComTratamento.java
+++ src/pilha/FluxoComTratamento.java
@@ -11,15 +11,13 @@ public class FluxoComTratamento {
 
             String msg = ex.getMessage();
 
-            System.out.println("Exception "+ msg);
+            System.out.println("Exception " + msg);
             ex.printStackTrace();
-
         }
         System.out.println("Fim do main");
     }
 
     private static void metodo1() {
-
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
@@ -29,12 +27,8 @@ public class FluxoComTratamento {
         System.out.println("Ini do metodo2");
         for (int i = 1; i <= 5; i++) {
             System.out.println(i);
-
             int a = i / 0;
-
         }
         System.out.println("Fim do metodo2");
     }
 }
-
-
